8a242c0cc8d0de68d65c3fddd6518469c2d467e5
Hamad <g.osmanov.muhammad@gmail.com>
2018-08-18 23:22:27 +0300

    Add display name for CounterHOC

--- src/views/HOC/counter/Counter.js
+++ src/views/HOC/counter/Counter.js
@@ -1,11 +1,12 @@
-import { compose, withState, withHandlers } from 'recompose'
+import { compose, withState, withHandlers, setDisplayName } from 'recompose'
 
 const withCounter = compose(
   withState('value', 'setValue', 0),
   withHandlers({
     inc: ({setValue}) => e => setValue(n => n + 1),
     dec: ({setValue}) => e => setValue(n => n - 1)
-  })
+  }),
+  setDisplayName('Counter')
 )
 
 export default withCounter
