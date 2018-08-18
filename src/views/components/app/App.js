935c587a1eed0720e2e9d1274b6e3f75e3e0666b
Hamad <g.osmanov.muhammad@gmail.com>
2018-08-18 11:32:21 +0300

    Change import order

--- src/views/components/app/App.js
+++ src/views/components/app/App.js
@@ -1,10 +1,11 @@
 import React, { Component, Fragment } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import CounterFromHOC from '../counter/CounterFromHOC'
 import Counter from '../counter/CounterRP'
+
+import logo from './logo.svg';
+import './App.css';
 import 'typeface-roboto'
 
 class App extends Component {
